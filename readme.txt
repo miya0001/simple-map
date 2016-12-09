f6dfdcaabf801eed10adcdd164a10631c05c14cd
miya0001 <miya@wpist.me>
2016-12-09 23:24:47 +0900

    tested on wp4.7

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: miyauchi
 Tags: google maps, map, shortcode, address
 Requires at least: 3.9
-Tested up to: 4.6
-Stable tag: 2.14.8
+Tested up to: 4.7
+Stable tag: 4.7.0
 
 Easy way to embed google map(s).
 
--- simple-map.php
+++ simple-map.php
@@ -4,7 +4,7 @@
  * Author: Takayuki Miyauchi
  * Plugin URI: https://github.com/miya0001/simple-map
  * Description: Insert google map convert from address.
- * Version: 2.14.8
+ * Version: 4.7.0
  * Author URI: http://wpist.me/
  * Text Domain: simple-map
  * Domain Path: /languages
